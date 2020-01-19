56293bd2ca17502beba1721b319b10900f648f6e
Student <nikitaadekar1798@gmail.com>
2020-01-19 11:31:08 -0500

    Updates

---
+++ precache-manifest.e55e6966ea8bf3c268fe6003dab6540e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dcda1fcfdbd33a1790561735df26bed",
+    "url": "/Image-Classification-AI/index.html"
+  },
+  {
+    "revision": "e91f2fa5bf30da506281",
+    "url": "/Image-Classification-AI/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9a5a4cd40f15d20f64fd",
+    "url": "/Image-Classification-AI/static/css/main.8c2c036a.chunk.css"
+  },
+  {
+    "revision": "e91f2fa5bf30da506281",
+    "url": "/Image-Classification-AI/static/js/2.36348418.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Image-Classification-AI/static/js/2.36348418.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a5a4cd40f15d20f64fd",
+    "url": "/Image-Classification-AI/static/js/main.318577b7.chunk.js"
+  },
+  {
+    "revision": "cd7ea04373889deaa3ff",
+    "url": "/Image-Classification-AI/static/js/runtime-main.85205ce3.js"
+  }
+]);
