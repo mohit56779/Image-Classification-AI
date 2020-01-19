82f94f86bfc0ddfe761c582e184603cad7d13033
nikitaadekar <nikitaadekar1798@gmail.com>
2020-01-19 05:11:55 -0500

    done methods for sign_up, image_id,profile

--- server.js
+++ server.js
@@ -1,49 +1,106 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = requre('bcrypt-nodejs')
+const cors = require('cors')
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users:[
-    {
-        id:"123",
-        name: 'john',
-        email: 'john@gmail.com',
-        password : 'cookie',
-        entries: 0,
-        joined: new Date()
-    },
-    {
+       {
+          id:"123",
+          name: 'john',
+          email: 'john@gmail.com',
+          password : 'cookie',
+          entries: 0,
+          joined: new Date()
+       },
+       {     
         id:"124",
-        name: 'sally',
-        email: 'sally@gmail.com',
-        password : 'bananas',
-        entries: 0,
-        joined: new Date()
+         name: 'sally',
+         email: 'sally@gmail.com',
+         password : 'bananas',
+         entries: 0,
+         joined: new Date()
     }
-
-    ]
+    ]    
 }
 
-app.get('/', (req,res)=> {
-    res.send('this is working');
+
+app.get('/', (req, res)=> {
+    res.send(database.users);
 })
+
 
 app.post('/signin',(req,res)=> {
     if(req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password)
-        {
+        req.body.password === database.users[0].password) {
             res.json('success');
+    } else {
+      res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+       id:'125',
+       name: name,
+       email: email,
+       password : password,
+       entries: 0,
+       joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return  res.json(user);
+
+        } 
+        
+        
+    })
+    if (!found) {
+        res.status(400).json('not found')
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return  res.json(user.entries);
         }
-        else{
-            
-            res.status('400').json('error logging in');
-        }
+         
+    })
+    if (!found) {
+        res.status(400).json('not found')
+    }
+    
+})
+app.listen(3000,()=>{
+    console.log('app is running on port 3000');
 })
 
 
-app.listen(3000,() => {
-    console.log("app is running on port 3000");
-})
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
