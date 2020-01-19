8602ed5b20a1c2ae1c6ab62b0d719ce5427c0fc8
Student <mohit56779@gmail.com>
2020-01-19 04:03:00 -0500

    inference works now. Clarifai api integraed, input box worksas well

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
 import React from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FoodRecognition from './components/FoodRecognition/FoodRecognition';
 import Rank from './components/Rank/Rank.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 
 
+
+
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '923853faa8cc48c683ebc28b4508f980'
+ });
 
 class App extends React.Component {
   constructor(){
     super();
     this.state={
-      input:""
+      input:"",
+      imageUrl: ""
     }
   }
 
   onInputChange = (event) =>{
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
+  }
+  infer = (concepts) =>{
+    console.log("hello");
+    var con_list;
+    con_list = [];
+    for(var i=0; i< concepts.length;i++){
+      console.log(i);
+      con_list.put(concepts[i].name)
+
+    }
+    console.log(con_list);
+  }
+  onButtonSubmit = () =>{
+    this.setState({imageUrl:this.state.input});
+    app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+      .then(generalModel => {
+        return generalModel.predict(this.state.input);
+      })
+      .then(response => {
+        
+        var concepts = response['outputs'][0]['data']['concepts'];
+        this.infer(concepts);
+
+
+      })
   }
 
-  onButtonSubmit = () =>{
-    console.log("click");
-    app.models.predict("bd367be194cf45149e75f01d59f77ba7", "https://samples.clarifai.com/food.jpg").then(
-      function(response) {
-      // do something with response
-    },
-      function(err) {
-      // there was an error
-    }
-  );
-  }
+ 
   render(){
   return (
     <div className="App">
@@ -38,8 +62,8 @@ class App extends React.Component {
       <Rank />
       <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit}/>
 
-      {/*}
-      <FaceRecognition />*/}
+  
+      <FoodRecognition imageUrl = {this.state.imageUrl} />
     </div>
   );
     }
